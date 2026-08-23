545e0b7f5ce446c8a82aca51acc747dc37881108
Mo <mo@iptv.com>
2026-08-23 22:12:27 +0000

    Update Trex server URL to mag.trexlive.me

--- worker/worker.js
+++ worker/worker.js
@@ -9,7 +9,7 @@
 
 const API_BASE    = "https://activationpanel.ru/api/api.php";
 const API_KEY     = "35cf68cc83a3a82e1a0ac5361c7b6105";
-const HOST        = "http://terry.thecontentnest.com";
+const HOST        = "http://mag.trexlive.me";
 const RESEND_KEY  = "re_QVAqR2F8_A9BDiVRQPjpPoFrt6fNpka2E";
 const FROM_EMAIL  = "Mojo 4K <kontakt@mojo4k.de>";
 const ADMIN_EMAIL = "kontakt@mojo4k.de";
